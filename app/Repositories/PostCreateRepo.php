befa702ac2e9b85b905544ebd377982035434844
Alfred Nutile <alfrednutile@gmail.com>
2020-11-12 06:30:23 -0500

    fix style

--- app/Repositories/PostCreateRepo.php
+++ app/Repositories/PostCreateRepo.php
@@ -9,18 +9,18 @@ use Illuminate\Support\Arr;
 class PostCreateRepo
 {
 
-  public function handle(PostCreate $payload)
-  {
-    $post = new Post();
-    $post->active = $payload->active;
-    $post->title = $payload->title;
-    $post->body = $payload->body;
-    $post->rendered_body = $payload->body;
-    $post->save();
-    //render the body
-    //check the schedule
-    //deal with tags
-    //return $post;
-    return $post;
-  }
+    public function handle(PostCreate $payload)
+    {
+        $post = new Post();
+        $post->active = $payload->active;
+        $post->title = $payload->title;
+        $post->body = $payload->body;
+        $post->rendered_body = $payload->body;
+        $post->save();
+      //render the body
+      //check the schedule
+      //deal with tags
+      //return $post;
+        return $post;
+    }
 }
